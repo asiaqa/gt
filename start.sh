373e01119d4beb988143d3c4ccdcc0be5c47151f
MP <112455607+asiaqa@users.noreply.github.com>
2022-09-11 00:27:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
 /gg -L=ss2+ws://AEAD_CHACHA20_POLY1305:$PASS@:81?path="/$MYPATH"&dns=127.0.0.1:$DNS/udp &> run.log& # | tee run.log&
 sed -i "s/\$DNS/$DNS/g" /ag/AdGuardHome.yaml
