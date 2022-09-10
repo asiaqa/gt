bd759445a9997657a94c4e9433171c4b404d9acc
MP <112455607+asiaqa@users.noreply.github.com>
2022-09-11 00:17:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
 /gg -L=ss2+ws://AEAD_CHACHA20_POLY1305:$PASS@:$PORT?path="/$MYPATH"&dns=127.0.0.1:$DNS/udp &> run.log& # | tee run.log&
 sed -i "s/\$DNS/$DNS/g" /ag/AdGuardHome.yaml
-/ag/adguard -c /ag/AdGuardHome.yaml -w /ag/ -l /ag/ag.log 
+cat /etc/caddy/Caddyfile | sed  -e "s/\$MYPATH/$MYPATH/g" -e "1c :$PORT" >/etc/caddy/Caddyfile
+/ag/adguard -c /ag/AdGuardHome.yaml -w /ag/ -l /ag/ag.log &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
 #if [ $DNS == 53 ]
 #then 
  
