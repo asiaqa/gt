a85e6c102f05db9081f908a2720b7d258ee05f88
MP <112455607+asiaqa@users.noreply.github.com>
2022-09-10 13:01:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
-/gg -L=ss2+ws://AEAD_CHACHA20_POLY1305:$PASS@:$PORT?path="/$MYPATH"&dns=127.0.0.1:$DNS/udp | tee run.log&
+/gg -L=ss2+ws://AEAD_CHACHA20_POLY1305:$PASS@:$PORT?path="/$MYPATH"&dns=127.0.0.1:$DNS/udp &> run.log& # | tee run.log&
 sed -i "s/\$DNS/$DNS/g" /ag/AdGuardHome.yaml
 /ag/adguard -c /ag/AdGuardHome.yaml -w /ag/ -l /ag/ag.log 
 #if [ $DNS == 53 ]
