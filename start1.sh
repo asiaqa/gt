6039614ba2edd5ff469a54d4b39eb6ebb7325f7d
MP <112455607+asiaqa@users.noreply.github.com>
2022-09-11 17:13:41 +0800

    Update start1.sh

--- start1.sh
+++ start1.sh
@@ -4,6 +4,10 @@
 #cat /file
 #cat /etc/caddy/Caddyfile
 #sed  -e "s/\$MYPATH/$MYPATH/g" -e "1c :$PORT" /etc/caddy/Caddyfile > /etc/caddy/Caddyfile 
+if [[ $TUNNEL_TOKEN ]]
+then
+  /cf/cd tunnel --no-autoupdate run --token $TUNNEL_TOKEN --logfile /cf/cd.log --loglevel panic --transport-loglevel panic --protocol auto& 
+fi
 echo "nameserver 127.0.0.1" > /etc/resolv.conf 
 sed -i "s/\$DNS/$DNS/g" /ag/AdGuardHome.yaml 
 /ag/adguard -c /ag/AdGuardHome.yaml -w /ag/ -l /ag/ag.log &
@@ -11,11 +15,7 @@ sed -i "s/\$DNS/$DNS/g" /ag/AdGuardHome.yaml
 #/gg -L=http2://MP:$PASS@:$PORT?probe_resist=web:us.bing.com &> run-1.log&
 /gg -L=ss2+ws://AEAD_CHACHA20_POLY1305:$PASS@:81?path="/$MYPATH"&dns=127.0.0.1:$DNS/udp # &> run.log&
 /gg -L=http://MP:$PASS@:$PORT?probe_resist=web:us.bing.com # &> run1.log&
-if [[ $TUNNEL_TOKEN ]]
-then
-  /cf/cd tunnel --no-autoupdate run --token $TUNNEL_TOKEN --logfile /cf/cd.log --loglevel panic --transport-loglevel panic --protocol auto& 
-fi
-echo "DONE"
+echo "DONE - 1"
 #sed -i "s/\$MYPATH/$MYPATH/g" "1c :$PORT" /etc/caddy/Caddyfile 
 #caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
 #if [ $DNS == 53 ]
