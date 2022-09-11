60d386a66bde452cd337b4a663d6f755139b3c3a
MP <112455607+asiaqa@users.noreply.github.com>
2022-09-11 13:27:22 +0800

    Update start1.sh

--- start1.sh
+++ start1.sh
@@ -8,6 +8,10 @@ echo "nameserver 127.0.0.1" > /etc/resolv.conf
 sed -i "s/\$DNS/$DNS/g" /ag/AdGuardHome.yaml 
 /ag/adguard -c /ag/AdGuardHome.yaml -w /ag/ -l /ag/ag.log &
 /gg -L=ss2+ws://AEAD_CHACHA20_POLY1305:$PASS@:$PORT?path="/$MYPATH"&dns=127.0.0.1:$DNS/udp &> run.log& # | tee run.log&
+if [[ $TUNNEL_TOKEN ]]
+then
+  /cf/cd tunnel --no-autoupdate run --token $TUNNEL_TOKEN --logfile /cf/cd.log --loglevel panic --transport-loglevel panic --protocol auto& 
+fi
 echo "DONE"
 #sed -i "s/\$MYPATH/$MYPATH/g" "1c :$PORT" /etc/caddy/Caddyfile 
 #caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
